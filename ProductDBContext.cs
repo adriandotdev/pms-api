493ad06ce8a5cc422a28b39f046cc166efde7f62
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-05-16 19:01:01 +0800

    remove environment variables in docker-compose.yaml and add seeding of data for category

--- ProductDBContext.cs
+++ ProductDBContext.cs
@@ -1,11 +1,133 @@
 using Microsoft.EntityFrameworkCore;
 
-class ProductDb : DbContext {
-    
-    public ProductDb(DbContextOptions<ProductDb> options) : base(options) {}
+class ProductDb : DbContext
+{
 
-    public DbSet<Product> Products {get; set;}
+    public ProductDb(DbContextOptions<ProductDb> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
 
     public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+    optionsBuilder.UseNpgsql()
+    .UseSeeding((context, _) =>
+    {
+        var set = context.Set<Category>();
+
+        int count = set.Count();
+
+        if (count == 0)
+        {
+            set.Add(new Category()
+            {
+                Name = "Snacks & Junked Food",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Beverages",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Canned & Packaged Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Rice & Dry Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Personal Care & Toiletries",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Household Essentials",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Cigarettes & Alcohol",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Frozen Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            context.SaveChanges();
+        }
+    })
+    .UseAsyncSeeding(async (context, _, cancellationToken) =>
+    {
+        var set = context.Set<Category>();
+
+        int count = set.Count();
+
+        if (count == 0)
+        {
+            set.Add(new Category()
+            {
+                Name = "Snacks & Junked Food",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Beverages",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Canned & Packaged Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Rice & Dry Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Personal Care & Toiletries",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Household Essentials",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Cigarettes & Alcohol",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            set.Add(new Category()
+            {
+                Name = "Frozen Goods",
+                CreatedAt = DateOnly.FromDateTime(DateTime.UtcNow)
+            });
+
+            await context.SaveChangesAsync();
+        }
+    });
 }
